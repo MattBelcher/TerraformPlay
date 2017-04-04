6dbe2eaaa3e8131bfe7cf4f4aab4944cd15a7e8f
Matt Belcher <mbelcher@thoughtworks.com>
2017-04-04 11:56:17 +0100

    Matt - Adding simple script to demonstrate how terraform can destroy infrastructures.

---
+++ destroy-infrastructure.sh
@@ -0,0 +1,7 @@
+# Example of how terraform can be used to completly destroy your infrastructure.
+
+# Shows what terraform would destroy...
+terraform plan -destroy
+
+# Performs the destroy
+terraform destroy
