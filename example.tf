daae397f56e4bcc7fcc502e51a9e73e266f76e7b
Matt Belcher <mbelcher@thoughtworks.com>
2017-04-04 14:25:08 +0100

    Matt - Adding an example of a basic provisioner.

--- example.tf
+++ example.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-c0998ca4"
 	instance_type = "t2.micro"
+
+	provisioner "local-exec" {
+		command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+	}
 }
 
 resource "aws_eip" "ip" {
